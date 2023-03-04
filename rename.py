ddc2411a99bf1f33199ff2769ea74680782ff4db
Leopard0001 <108943378+Leopard0001@users.noreply.github.com>
2023-03-04 19:39:30 +0300

    Update rename.py

--- rename.py
+++ rename.py
@@ -2,5 +2,8 @@ import os
 
 out = input("Введите часть для удаления из названия файлов >>> ")
 
-for file in os.listdir():
-    os.rename(file, file.replace(out,""))
+i = len(os.listdir())
+while i>0:
+    for file in os.listdir():
+        os.rename(file, file.replace(out,""))
+    i=-1
